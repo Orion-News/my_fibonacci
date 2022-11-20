9d3dbb7df85c478570f82828381b4789eead1326
Orion-News <geeh-eb@hotmail.com>
2022-11-19 22:21:38 -0300

    refactor: changed data structure

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ class Fibonacci {
 		];
 	*/
 	constructor(term) {
-		this.penultimate = 0; 
-		this.last = 1;
-		this.number;
-		this.listFibonacci = [];
 		this.Init(term);
 	};
 
@@ -20,19 +16,20 @@ class Fibonacci {
 	Init(term) {
 		// valida o valor inserido no input 'termo' para ver se existe sequencia;
 		if(term <= 2) {
-			this.number = term -1;
+		        process.stdout.write(`Not Valid Value`);
 		   	} else {
-				// Fibonacci sequence;
-				// inserir 0;
-				this.listFibonacci.push(this.penultimate);
-				// inserir 1;
-				this.listFibonacci.push(this.last);
+				// Fibonacci sequence, definitions of variables;
+                this.listFibonacci = '0 1';
+                this.penultimate = 0;
+                this.last = 1;
+                this.number;
+
 				// loop para gerar a sequencia a partir do termo;
 				for(let count = 3; count <= term; count++) {
 					// termo subsequente corresponde à soma dos dois anteriores;
 					this.number = this.last + this.penultimate;
 					// inserir na lista
-					this.listFibonacci.push(this.number);
+					this.listFibonacci += ` ${this.number}`;
 					// definir penultimo número como igual ao ultimo;
 					this.penultimate = this.last;
 					// definir ultimo igual ao correspondente a soma dos anteriores;
@@ -48,26 +45,48 @@ class Fibonacci {
 			(List) -> valor na memoria sem ter contato com a lista original;
 			
 			DESCRIPTION
-			{ CalculateSum }: responsavel por somar todos os valores com uma referencia da lista original,
-				adiciona a soma de todos os valores na list(referenciada) inverte a lista do menor para o maior,
-				recorta os 4 primeiros valores dessa lista e envia para a DisplayValue responsavel,
-				por exibir os valores formatados;
+			{ CalculateSum }: Recebe uma String, 
+                divide os valores em uma lista separada da String original;
+                calcula a soma de todos os valores da lista separada;
+                atribui a soma da lista separada a String original;
 		];
 	*/
 	CalculateSum(list) {
-		// calcula a soma de todos os valores inseridos na variável (this.listFibonacci);
-		let reduceList = list.reduce((a, b) => {
-			return a + b
+        // transforma a string em uma lista;
+        let splintInList = list.split(' ');
+
+        // calcula a soma de todos os valores da lista dividida;
+		let reduceList = splintInList.reduce((a, b) => {
+            return parseInt(a) + parseInt(b);
 		});
-		// inserir a soma de todos os valores na lista;
-		list.push(reduceList);
-		// criando sequencia inversa, do maior para o menor;
-		let reverseList = list.reverse();
-		// cortar list nos 4 primeiros números da sequencia invertida;
-		let listSlice = reverseList.slice(0, 4);
-		// chamar função para exibir valores em uma string com espaço;
-		this.DisplayValue(listSlice);
+
+        // atribui a soma da lista dividida a string original;
+        list += ` ${reduceList}`;
+
+        // passa valor para função que vai formatar a String;
+		this.FormatValue(list);
 	};
+
+    /* 
+		-:{ FormatValue }:- [
+			(list) -> sequencia Fibonacci + a soma de todos os termos em uma string,
+				utiliza de metodos já criados no javascript para reverte e formatar uma string;
+
+			DESCRIPTION
+			{ FormatValue }: responsavel por utilizar metodos internos da linguagem para reverter,
+				os valores da sequencia Fibonncci de trás pra frente, e recortar para facilitar,
+                o ultimo processo de exibição de valores geradors a partir do termo de entrada;
+		];
+	*/
+    FormatValue(list) {
+        let splitInList = list.split(' ');
+
+        let reverseList = splitInList.reverse();
+
+        let sliceList = reverseList.slice(0, 4);
+
+        this.DisplayValue(sliceList);
+    };
 
 	/* 
 		-:{ DisplayValue }:- [
@@ -82,16 +101,17 @@ class Fibonacci {
 	DisplayValue(listSlice) {
 		// criando uma variável do tipo string para receber os pedaços da fatia;
 		let outputResult = '';
+        
 		// loop para separar os pedaços por espaço e inserindo no resultado de saida;
 		for (let sliceCount = 0; sliceCount < listSlice.length; sliceCount++) {
-			outputResult += `${listSlice[sliceCount]} `;
+            outputResult += `${listSlice[sliceCount]} `;
 		};
+
 		// exibindo o resultado de saida;
 		process.stdout.write(outputResult);
 	};
 };
 
 
-// chamada da class: hmmmm é mesmo ?
-// se o valor for 2 não vai exibir nada;
+// termo de entrada;
 new Fibonacci(10);
